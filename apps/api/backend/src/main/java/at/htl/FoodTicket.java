152bc2ebe949326df9d5f51275dbfc23bea2a2e4
Julian Richter <julian.paul.richter@gmail.com>
2026-07-06 14:33:24 +0200

    #3 Essensmarken Entity angelegt

---
+++ apps/api/backend/src/main/java/at/htl/FoodTicket.java
@@ -0,0 +1,16 @@
+package at.htl;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Essensmarke {
+
+    @Id
+    private Long id;
+
+    private LocalDateTime Datum
+}
