04ee2d73a47e42dd01068f2ddb355c7bf4a9032b
Julian Richter <julian.paul.richter@gmail.com>
2026-07-06 14:33:57 +0200

    #Essensmarken Entity angelegt

--- apps/api/backend/src/main/java/at/htl/FoodTicket.java
+++ apps/api/backend/src/main/java/at/htl/FoodTicket.java
@@ -2,15 +2,52 @@ package at.htl;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-public class Essensmarke {
+public class FoodTicket {
 
     @Id
     private Long id;
 
-    private LocalDateTime Datum
+    private LocalDateTime Date;
+
+    @OneToOne
+    private Employee employee;
+
+    private int costOrder;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return Date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        Date = date;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public int getCostOrder() {
+        return costOrder;
+    }
+
+    public void setCostOrder(int costOrder) {
+        this.costOrder = costOrder;
+    }
 }
