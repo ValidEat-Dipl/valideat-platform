41ef1d7f68cdcc979a424f7252a9b7d17ba6768e
Julian Richter <julian.paul.richter@gmail.com>
2026-07-08 15:53:00 +0200

    #8 Login und Register Methoden im Backend

--- apps/api/backend/src/main/java/at/htl/model/Employee.java
+++ apps/api/backend/src/main/java/at/htl/model/Employee.java
@@ -1,19 +1,40 @@
 package at.htl.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
 
     private String lastName;
 
+    private String address;
 
+    private String department;
+
+    private String phoneNumber;
+
+    private String email;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String address, String department, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.department = department;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 
     public Long getId() {
         return id;
@@ -37,5 +58,37 @@ public class Employee {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
